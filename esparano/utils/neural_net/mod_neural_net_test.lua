efcce5a8959252ec01db1b19783ce1e4db0ef086
Evan Sparano <evansparano@gmail.com>
2018-09-23 02:38:14 -0400

    neural-net: test with multiple layers

--- esparano/utils/neural_net/mod_neural_net_test.lua
+++ esparano/utils/neural_net/mod_neural_net_test.lua
@@ -66,7 +66,7 @@ function test_predict_with_bias_real()
             {0.04400307, 0.01574006},
             {0.04959274, 0.04452698},
             {0.04298705, -0.04140915},
-            {0.01, 0.01}
+            {0.01, 0.01} -- bias
         }
     }
     local expected = {0.41366219, 0}
@@ -74,6 +74,30 @@ function test_predict_with_bias_real()
     local actual = net:predict(input)
     for i = 1, #expected do
         assert.equals(round(expected[i], 7), round(actual[i], 7))
+    end
+end
+
+function test_predict_multiple_layers_and_bias()
+    local input = {1, 2, 3, 4}
+    local weights = {
+        {
+            {1, 5},
+            {2, 6},
+            {3, 7},
+            {4, 8},
+            {1, 1}
+        },
+        {
+            {3},
+            {4},
+            {0.123}
+        }
+    }
+    local expected = {377.123}
+    local net = nn.new(weights, true)
+    local actual = net:predict(input)
+    for i = 1, #input do
+        assert.equals(expected[i], actual[i])
     end
 end
 
