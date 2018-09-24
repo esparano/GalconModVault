87b42b315697dd8e4333ed7537c1830fbad1e92e
Evan Sparano <evansparano@gmail.com>
2018-09-24 00:08:47 -0400

    profiler: initial commit

---
+++ esparano/utils/profiler/mod_profiler.lua
@@ -0,0 +1,48 @@
+-- TODO: documentation
+function _profiler_init()
+    local profiler = {}
+
+    function profiler.new()
+        local instance = {}
+        for k, v in pairs(profiler) do
+            instance[k] = v
+        end
+        instance.data = {}
+        return instance
+    end
+
+    local function trackCall(self, obj, funcName, elapsed)
+        local data = self.data[obj][funcName]
+        data.n = data.n + 1
+        data.elapsed = data.elapsed + elapsed
+    end
+
+    local function getTrackedFunc(self, obj, funcName, func)
+        return function(...)
+            local start = os.clock()
+            local result = func(...)
+            trackCall(self, obj, funcName, os.clock() - start)
+            return result -- DOES NOT WORK FOR MULTIPLE RETURN VALUES
+        end
+    end
+
+    -- note: functions on obj cannot change after this point or they won't be tracked
+    function profiler:profile(obj)
+        self.data[obj] = {}
+        for k, v in pairs(obj) do
+            if type(v) == "function" then
+                obj[k] = getTrackedFunc(self, obj, k, v)
+                self.data[obj][k] = {n = 0, elapsed = 0}
+            end
+        end
+    end
+
+    -- data[obj].funcName.n = num calls, data[obj].funcName.elapsed = total elapsed time for n calls 
+    function profiler:getData()
+        return self.data
+    end
+
+    return profiler
+end
+profiler = _profiler_init()
+_profiler_init = nil
---
+++ esparano/utils/profiler/mod_profiler_test.lua
@@ -0,0 +1,57 @@
+require("mod_assert")
+require("mod_profiler")
+
+function test_wraps_multiarg_function()
+    local testModule = {}
+    local actualX, actualY
+    testModule.f = function(x, y) 
+        actualX, actualY = x, y
+        for i=1,(x+y) do
+            local a = 1 + i
+        end
+    end
+
+    local profiler = profiler.new()
+    profiler:profile(testModule)
+
+    local expectedX = 1235
+    local expectedY = 1513135
+    testModule.f(expectedX, expectedY)
+    assert.equals(expectedX, actualX)
+    assert.equals(expectedY, actualY)
+end
+
+function test_profiling_data() 
+    local testModule = {}
+    testModule.f = function() 
+        for i=1,100000 do
+            local a = 1 + i
+        end
+    end
+
+    local profiler = profiler.new()
+    profiler:profile(testModule)
+    local data = profiler:getData()
+    
+    testModule.f()
+
+    local elapsed = data[testModule].f.elapsed
+    assert.equals(1, data[testModule].f.n)
+    assert.not_equals(0, elapsed)
+    
+    testModule.f()
+    
+    assert.equals(2, data[testModule].f.n)
+    assert.is_true(data[testModule].f.elapsed > elapsed)
+end
+
+function init()
+end
+
+function loop(t)
+end
+
+function event(e)
+end
+
+require("mod_test_runner")
