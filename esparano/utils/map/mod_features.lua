aea0f551de7a20577bd025e7724c37c3a53af238
Evan Sparano <evansparano@gmail.com>
2018-11-11 01:59:16 -0500

    switching branches

--- esparano/utils/map/mod_features.lua
+++ esparano/utils/map/mod_features.lua
@@ -4,6 +4,9 @@ require("mod_map")
 function _features_init()
     local features = {}
 
+    local PROD_COM_DIST_NORMALIZATION_FACTOR = 300
+    local SHIPS_COM_DIST_NORMALIZATION_FACTOR = 300
+
     function features.prodFraction(map, user, enemy)
         local userProd = map:totalProd(user.n)
         local enemyProd = map:totalProd(enemy.n)
@@ -39,14 +42,14 @@ function _features_init()
     function features.prodCenterOfMassDistance(map, user, enemy)
         local userCOM = centerOfMass(map:getPlanetList(user.n), "production")
         local enemyCOM = centerOfMass(map:getPlanetList(enemy.n), "production")
-        return cOMDistance(userCOM, enemyCOM)
+        return cOMDistance(userCOM, enemyCOM) / PROD_COM_DIST_NORMALIZATION_FACTOR
     end
 
     -- includes fleets
     function features.shipsCenterOfMassDistance(map, user, enemy)
         local userCOM = centerOfMass(map:getPlanetAndFleetList(user.n), "ships")
         local enemyCOM = centerOfMass(map:getPlanetAndFleetList(enemy.n), "ships")
-        return cOMDistance(userCOM, enemyCOM)
+        return cOMDistance(userCOM, enemyCOM) / SHIPS_COM_DIST_NORMALIZATION_FACTOR
     end
 
     function features.getAll(map, user, enemy)
