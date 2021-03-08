9272b73097ab67091298f8520ad990067859c4db
Evan Sparano <evansparano@gmail.com>
2021-03-08 01:28:38 -0500

    mod_test_runner: minor working tweak

--- esparano/utils/test_runner/mod_test_runner.lua
+++ esparano/utils/test_runner/mod_test_runner.lua
@@ -4,7 +4,7 @@ function _run_tests()
     end
     for f_name, f in pairs(_ENV) do
         if f_name:sub(1, 5) == "test_" then
-            print("running test: " .. f_name)
+            print("___RUNNING_TEST___: " .. f_name)
             if before_each then
                 before_each()
             end
